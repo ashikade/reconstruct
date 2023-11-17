b12222284272848685232bb01d8bdd48358d1a74
Ashika Devi <129720367+ashikade@users.noreply.github.com>
2023-11-17 15:06:24 +0530

    Update make-me-smile.js

--- js/make-me-smile.js
+++ js/make-me-smile.js
@@ -1,9 +1,14 @@
 let maxSmileyChanges = 12;
 let currentSmiley = 2;
 
-function changeSmiley() {
+function changeSmiley(audioFile) {
     if (currentSmiley <= maxSmileyChanges) {
         document.getElementById('smiley').src = `images/smiley${currentSmiley}.png`;
+
+        // Play the audio file
+        let audio = new Audio(audioFile);
+        audio.play();
+
         currentSmiley++;
     }
 }
